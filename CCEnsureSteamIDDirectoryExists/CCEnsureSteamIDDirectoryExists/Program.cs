870f39957d431061a8ed0ca6d72b0e48dfea1c20
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2014-07-13 01:34:44 -0400

    Fixed broken logic
    Fixed incorrect assumption that puzzles was in root directory

--- CCEnsureSteamIDDirectoryExists/CCEnsureSteamIDDirectoryExists/Program.cs
+++ CCEnsureSteamIDDirectoryExists/CCEnsureSteamIDDirectoryExists/Program.cs
@@ -9,18 +9,20 @@ namespace CCEnsureSteamIDDirectoryExists
         static void Main()
         {
             string problem = string.Empty;
-            // Traverse up out of the bin directory, then navigate to aperturetag\puzzles, copy the names of all the directories in there, and make new directories with the same names in aperturetag\portal2\maps\puzzlemaker
-            var apertureTag = Directory.GetParent(Directory.GetCurrentDirectory());
+            // Traverse up out of the bin directory, then navigate to \aperturetag\puzzles, copy the names of all the directories in there, and make new directories with the same names in \portal2\maps\puzzlemaker
+            var root = Directory.GetParent(Directory.GetCurrentDirectory());
 
+            var apertureTag = root.GetDirectories("aperturetag").FirstOrDefault();
+            if (apertureTag == null) { LogProblem("Failed to find '\\aperturetag'"); return; }
             var puzzles = apertureTag.GetDirectories("puzzles").FirstOrDefault();
-            if (puzzles == null) { LogProblem("Failed to find 'aperturetag\\puzzles'"); return; }
+            if (puzzles == null) { LogProblem("Failed to find '\\aperturetag\\puzzles'"); return; }
 
             var portal2 = apertureTag.GetDirectories("portal2").FirstOrDefault();
-            if (portal2 == null) { LogProblem("Failed to find 'aperturetag\\portal2'"); return; }
+            if (portal2 == null) { LogProblem("Failed to find '\\portal2'"); return; }
             var maps = portal2.GetDirectories("maps").FirstOrDefault();
-            if (maps == null) { LogProblem("Failed to find 'aperturetag\\portal2\\maps'"); return; }
+            if (maps == null) { LogProblem("Failed to find '\\portal2\\maps'"); return; }
             var puzzlemaker = maps.GetDirectories("puzzlemaker").FirstOrDefault();
-            if (puzzlemaker == null) { LogProblem("Failed to find 'aperturetag\\portal2\\maps\\puzzlemaker'"); return; }
+            if (puzzlemaker == null) { LogProblem("Failed to find '\\portal2\\maps\\puzzlemaker'"); return; }
 
             foreach (var directory in puzzles.EnumerateDirectories())
                 if (puzzlemaker.EnumerateDirectories().Where(d => d.Name == directory.Name).FirstOrDefault() == null)
